4fdc0167fa58175136a6d0bb8f744e080c07788f
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-09-03 00:37:52 -0300

    feat: create users router structure

---
+++ src/modules/users/index.js
@@ -0,0 +1,15 @@
+export const list = () => {
+  return 'List'
+}
+
+export const create = () => {
+  return 'Create'
+}
+
+export const update = () => {
+  return 'Update'
+}
+
+export const remove = () => {
+  return 'Remove'
+}
--- src/router.js
+++ src/router.js
@@ -1,5 +1,12 @@
 import Router from '@koa/router'
 
+import * as usersRouter from './modules/users'
+
 const router = new Router()
+
+router.get('/users', usersRouter.list)
+router.post('/users', usersRouter.create)
+router.put('/users', usersRouter.update)
+router.delete('/users', usersRouter.remove)
 
 export default router
