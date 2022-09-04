04c96ee1ac63e35c0da7b6e73591d1462db68f4a
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-09-04 20:37:03 -0300

    feat: implement users crud

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -1,15 +1,68 @@
-export const list = () => {
-  return 'List'
+import { prisma } from '~/data'
+
+export const list = async ctx => {
+  try {
+    const users = await prisma.user.findMany()
+
+    ctx.body = users
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = 'Ops! Algo deu errado, tente novamente.'
+  }
 }
 
-export const create = () => {
-  return 'Create'
+export const create = async ctx => {
+  const { name, email, password } = ctx.request.body
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+
+    ctx.body = user
+  } catch (error) {
+    console.log(error)
+    ctx.status = 500
+    ctx.body = 'Ops! Algo deu errado, tente novamente.'
+  }
 }
 
-export const update = () => {
-  return 'Update'
+export const update = async ctx => {
+  const { name, email } = ctx.request.body
+
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: ctx.params.id,
+      },
+      data: {
+        name,
+        email,
+      },
+    })
+
+    ctx.body = user
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = 'Ops! Algo deu errado, tente novamente.'
+  }
 }
 
-export const remove = () => {
-  return 'Remove'
+export const remove = async ctx => {
+  try {
+    await prisma.user.delete({
+      where: {
+        id: ctx.params.id,
+      },
+    })
+
+    ctx.body = { id: ctx.params.id }
+  } catch (error) {
+    ctx.status = 500
+    ctx.body = 'Ops! Algo deu errado, tente novamente.'
+  }
 }
--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const router = new Router()
 
 router.get('/users', usersRouter.list)
 router.post('/users', usersRouter.create)
-router.put('/users', usersRouter.update)
-router.delete('/users', usersRouter.remove)
+router.put('/users/:id', usersRouter.update)
+router.delete('/users/:id', usersRouter.remove)
 
 export default router
