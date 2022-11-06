bc7895baeaa3c3d393a76fbc5904a8a6484b57fb
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2022-11-06 00:26:58 -0300

    feat: enable cors using @koa/cors package

--- src/server-setup.js
+++ src/server-setup.js
@@ -1,9 +1,12 @@
 import Koa from 'koa'
-import router from './router'
 import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+
+import router from './router'
 
 const app = new Koa()
 
+app.use(cors())
 app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
